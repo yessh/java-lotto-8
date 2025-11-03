c59f2ed6fa8d94cefc22955d1d305ebed56d2f53
yessh <keh1058@gmail.com>
2025-11-03 22:52:44 +0900

    Refactor(Output): 최종 결과(등수, 수익률) 출력 로직 추가

--- src/main/java/lotto/view/Output.java
+++ src/main/java/lotto/view/Output.java
@@ -2,6 +2,10 @@ package lotto.view;
 
 import lotto.model.Lotto;
 import lotto.model.Lottos;
+import lotto.model.Rank;
+import lotto.model.Statistics;
+
+import java.util.Map;
 
 public class Output {
 
@@ -13,5 +17,22 @@ public class Output {
         for (Lotto lotto : lottos.getLottos()) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void printStatistics(Statistics statistics) {
+        System.out.println("\n당첨 톰계");
+        System.out.println("---");
+
+        Map<Rank, Integer> result = statistics.getResult();
+
+        System.out.printf("3개 일치 (%,d원) - %d개\n", Rank.FIFTH.getPrizeMoney(), result.get(Rank.FIFTH));
+        System.out.printf("4개 일치 (%,d원) - %d개\n", Rank.FOURTH.getPrizeMoney(), result.get(Rank.FOURTH));
+        System.out.printf("5개 일치 (%,d원) - %d개\n", Rank.THIRD.getPrizeMoney(), result.get(Rank.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치 (%,d원) - %d개\n", Rank.SECOND.getPrizeMoney(), result.get(Rank.SECOND));
+        System.out.printf("6개 일치 (%,d원) - %d개\n", Rank.FIRST.getPrizeMoney(), result.get(Rank.FIRST));
+    }
+
+    public void printProfitability(double profitability) {
+        System.out.printf("총 수익률은 %.1f%%입니다.\n", profitability);
     }
 }
